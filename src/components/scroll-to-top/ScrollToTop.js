5b383079ac94c8001d483d9c56b9b075f5834c5f
fastbac <fastbac65.gmail.com>
2023-05-25 13:26:50 +1000

    fixed proptypes

--- src/components/scroll-to-top/ScrollToTop.js
+++ src/components/scroll-to-top/ScrollToTop.js
@@ -1,39 +1,46 @@
+import PropTypes from 'prop-types';
+
 // ** MUI Imports
-import Zoom from '@mui/material/Zoom'
-import { styled } from '@mui/material/styles'
-import useScrollTrigger from '@mui/material/useScrollTrigger'
+import Zoom from '@mui/material/Zoom';
+import { styled } from '@mui/material/styles';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
 const ScrollToTopStyled = styled('div')(({ theme }) => ({
   zIndex: 11,
   position: 'fixed',
   right: theme.spacing(6),
-  bottom: theme.spacing(10)
-}))
+  bottom: theme.spacing(10),
+}));
 
-const ScrollToTop = props => {
+const ScrollToTop = (props) => {
   // ** Props
-  const { children, className } = props
+  const { children, className } = props;
 
   // ** init trigger
   const trigger = useScrollTrigger({
     threshold: 400,
-    disableHysteresis: true
-  })
+    disableHysteresis: true,
+  });
 
   const handleClick = () => {
-    const anchor = document.querySelector('body')
+    const anchor = document.querySelector('body');
     if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth' })
+      anchor.scrollIntoView({ behavior: 'smooth' });
     }
-  }
+  };
 
   return (
     <Zoom in={trigger}>
-      <ScrollToTopStyled className={className} onClick={handleClick} role='presentation'>
+      <ScrollToTopStyled className={className} onClick={handleClick} role="presentation">
         {children}
       </ScrollToTopStyled>
     </Zoom>
-  )
-}
+  );
+};
 
-export default ScrollToTop
+ScrollToTop.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default ScrollToTop;
