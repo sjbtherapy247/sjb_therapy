fc2f9d48778280c5dc678375794f2abab551a7f2
fastbac <fastbac65.gmail.com>
2023-06-15 19:19:56 +1000

    x

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,5 +1,7 @@
 // next
 import Head from 'next/head';
+// stripe
+// import Stripe from 'stripe';
 // layouts
 import MainLayout from 'src/layouts/main';
 // sections
@@ -7,6 +9,20 @@ import { ServicesView } from 'src/sections/_simo/view';
 
 import { servicesDescription } from 'src/sections/_simo/services/svcs';
 import { sessionPricing } from 'src/sections/_simo/pricing/pricing';
+
+// export async function getServerSideProps() {
+//   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+//   const { data: prices } = await stripe.prices.list({
+//     active: true,
+//     limit: 10,
+//     expand: ['data.product'],
+//   });
+//   return {
+//     props: {
+//       prices,
+//     },
+//   };
+// }
 
 export async function getStaticProps() {
   console.log(servicesDescription);
@@ -23,7 +39,8 @@ ServicesPage.getLayout = (page) => <MainLayout>{page}</MainLayout>;
 
 // ----------------------------------------------------------------------
 
-export default function ServicesPage({ servicesDocs, pricing }) {
+export default function ServicesPage({ servicesDocs, pricing, prices }) {
+  console.log(prices);
   return (
     <>
       <Head>
