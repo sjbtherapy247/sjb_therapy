6e84cc729548628eefea7b185d6af5c3570af1ca
fastbac <fastbac65.gmail.com>
2023-06-17 18:54:09 +1000

    x

--- src/sections/_simo/pricing/PricingHome.js
+++ src/sections/_simo/pricing/PricingHome.js
@@ -140,7 +140,7 @@ export default function PricingHome({ plans, prices }) {
               {plans.map((plan) => (
                 <Box key={plan.license} sx={{ position: 'relative', px: 1, py: { xs: 8, md: 10 } }}>
                   {/* <m.div key={plan.license} variants={varFade({ distance: 240 }).inDown}> */}
-                  <PlanCard plan={plan} />
+                  <PlanCard plan={plan} prices={prices} />
                   {/* </m.div> */}
                 </Box>
               ))}
