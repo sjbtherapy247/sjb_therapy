ae5443ae515352485c5aead2eb8134c85218bc60
fastbac <fastbac65.gmail.com>
2023-06-18 03:08:11 +1000

    x

--- src/sections/_simo/view/HomeView.js
+++ src/sections/_simo/view/HomeView.js
@@ -1,24 +1,4 @@
-// _mock
-import { research } from 'src/sections/_simo/insights/articles';
-// components
-import ScrollProgress from 'src/components/scroll-progress';
-//
-// import { useSettingsContext } from 'src/components/settings';
-import PricingHome from 'src/sections/_simo/pricing';
-import {
-  HomeHero,
-  HomeFAQs,
-  // HomeNewStart,
-  // HomeForDesigner,
-  // HomeCombination,
-  // HomeAdvertisement,
-  HomeGuidedMeditation,
-  HomeFamous,
-  HomeBanner,
-  // HomeFeatureHighlights,
-  // HomeFlexibleComponents,
-} from 'src/sections/_simo/home';
-// import TestFooter from 'src/layouts/main/footer/TestFooter';
+import { HomeHero, HomeFAQs, HomeGuidedMeditation, HomeFamous, HomeBanner } from 'src/sections/_simo/home';
 import { LatestPosts } from '../insights';
 
 // ----------------------------------------------------------------------
