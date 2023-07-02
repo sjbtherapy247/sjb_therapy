5fe11371696d0fedbe3bfba93cf86a14deb33ddc
fastbac <fastbac65.gmail.com>
2023-07-03 00:20:19 +1000

    x

--- src/layouts/main/footer/Footer.js
+++ src/layouts/main/footer/Footer.js
@@ -1,18 +1,70 @@
 // next
 import NextLink from 'next/link';
+// fb
+import { db } from 'src/lib/createFirebaseApp';
+import { update, ref, serverTimestamp, get } from 'firebase/database';
+// RHF
+import * as Yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 // mui
-import { Box, Button, Container, Divider, Unstable_Grid2 as Grid, IconButton, InputAdornment, Link, Stack, TextField, Typography, alpha, useTheme } from '@mui/material';
+import { Box, Button, Container, Divider, FormControl, FormHelperText, Unstable_Grid2 as Grid, IconButton, InputAdornment, Link, Stack, TextField, Typography, alpha, useTheme } from '@mui/material';
 import { _socialsSimo } from 'src/_mock';
 import Iconify from 'src/components/iconify/Iconify';
+import { useRef, useState } from 'react';
+import { useSettingsContext } from 'src/components/settings';
+import { RHFTextField } from 'src/components/hook-form';
+import FormProvider from 'src/components/hook-form/FormProvider';
+import LoadingButton from 'src/theme/overrides/LoadingButton';
 
 const Footer = () => {
   // something
   const theme = useTheme();
 
+  const {
+    dispatch,
+    state: { alert, modal },
+    // clients,
+  } = useSettingsContext();
+
+  const ResetPasswordSchema = Yup.object().shape({
+    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    fname: Yup.string().required('First name is required').max(20).min(3),
+  });
+  const methods = useForm({
+    resolver: yupResolver(ResetPasswordSchema),
+    defaultValues: {
+      email: '',
+      fname: '',
+    },
+  });
+  const {
+    reset,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = (data) => {
+    // all new subscriptions are stored under a timestamp
+    get();
+    update(ref(db, `InSights/${Date.now()}`), { first_name: data.fname, email: data.email, timestamp: serverTimestamp() });
+    dispatch({
+      type: 'UPDATE_ALERT',
+      payload: {
+        ...alert,
+        open: true,
+        severity: 'success',
+        message: `Thank you ${data.fname} for subscribing to InSights.  ${data.email} is registered.`,
+        duration: 5000,
+        posn: 'bottom',
+      },
+    });
+  };
+
   return (
     <>
       <Grid container justifyContent={{ md: 'space-between' }}>
-        <Grid xs={12} md={5.5} sx={{ p: 4, bgcolor: 'background.neutral', display: 'flex', alignItems: 'center' }}>
+        <Grid xs={12} md={5.5} sx={{ p: 4, bgcolor: 'background.neutral', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
           <Stack spacing={{ xs: 3, md: 5 }}>
             <Box display="grid" gridTemplateColumns="repeat(3, 1fr)">
               <Stack spacing={1} alignItems="center">
@@ -67,25 +119,30 @@ const Footer = () => {
                 The newsletter that will inspire you to continue your improvement journey.
               </Typography>
             </Stack>
-            <Stack spacing={1} direction={{ xs: 'column', sm: 'row' }} justifyContent="center">
-              <TextField hiddenLabel required placeholder="First Name" sx={{ minWidth: 160 }} />
-              <TextField
-                fullWidth
-                hiddenLabel
-                placeholder="Enter your email"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <Button size="large" color="primary" variant="contained" sx={{ p: 0, height: 53, borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}>
-                        <Iconify icon="carbon:send" />
-                      </Button>
-                    </InputAdornment>
-                  ),
-                  sx: { pr: 0 },
-                }}
-                // sx={{ maxWidth: 360 }}
-              />
-            </Stack>
+            <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+              <Stack spacing={1} direction={{ xs: 'column', sm: 'row' }} justifyContent="center">
+                <RHFTextField name="fname" hiddenLabel placeholder="First Name" sx={{ minWidth: 160, maxWidth: 180 }} />
+                <RHFTextField
+                  name="email"
+                  fullWidth
+                  aria-label="email"
+                  hiddenLabel
+                  placeholder="Enter your email"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Button type="submit" size="large" color="primary" variant="contained" sx={{ p: 0, height: 53, borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}>
+                          <Iconify icon="carbon:send" />
+                        </Button>
+                      </InputAdornment>
+                    ),
+                    sx: { pr: 0 },
+                  }}
+                  // sx={{ maxWidth: 360 }}
+                />
+              </Stack>
+              {/* <FormHelperText id="my-helper-text">We will never share your details.</FormHelperText> */}
+            </FormProvider>
           </Stack>
 
           <Stack direction="row" alignItems="center" justifyContent="center" sx={{ pt: 4 }}>
