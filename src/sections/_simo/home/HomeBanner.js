3ffe2cbd4561b62dc5561ff00962610a2d9b885e
sjbtherapy247 <sjbtherapy247@gmail.com>
2023-08-16 16:51:46 +1000

    homepage banner tweak

--- src/sections/_simo/home/HomeBanner.js
+++ src/sections/_simo/home/HomeBanner.js
@@ -8,7 +8,7 @@ import Markdown from 'src/components/markdown/Markdown';
 // ----------------------------------------------------------------------
 
 export default function HomeBanner({ sx, ...other }) {
-  const quote = `<blockquote>Make your problems become your strengths</blockquote> `;
+  const quote = `<blockquote>Turn your problems into your strengths with Hypnotherapy</blockquote> `;
   return (
     <Box sx={{ py: 4, bgcolor: 'background.neutral' }} {...other}>
       <Container>
