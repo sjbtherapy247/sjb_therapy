8e42638f32c4923fd3d24e3603160d82d1872c2d
sjbtherapy247 <sjbtherapy247@gmail.com>
2023-08-21 15:11:03 +1000

    trying...

--- src/sections/_simo/home/HomeBanner.js
+++ src/sections/_simo/home/HomeBanner.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 // @mui
 import { Box, Stack, Button, Container, TextField, Typography, InputAdornment } from '@mui/material';
+import { MotionViewport, varFade } from 'src/components/animate';
 // components
 import SvgColor from 'src/components/svg-color';
 import Markdown from 'src/components/markdown/Markdown';
@@ -11,7 +12,15 @@ export default function HomeBanner({ sx, ...other }) {
   const quote = `<blockquote>Turn your problems into your strengths with Hypnotherapy</blockquote> `;
   return (
     <Box sx={{ py: 4, bgcolor: 'background.neutral' }} {...other}>
-      <Container>
+      <Container
+      component={MotionViewport}
+      sx={{
+        textAlign: 'center',
+        pt: { xs: 5, md: 10 },
+        pb: { xs: 5, md: 15 },
+      }}
+      >
+      
         <Markdown content={quote} />
       </Container>
     </Box>
