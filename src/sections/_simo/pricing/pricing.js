d8c867198485cd9165f6f83aff1b2ee751c56b93
sjbtherapy247 <sjbtherapy247@gmail.com>
2023-12-08 17:35:02 +1100

    more price

--- src/sections/_simo/pricing/pricing.js
+++ src/sections/_simo/pricing/pricing.js
@@ -7,19 +7,20 @@ export const sessionPricing = [
     options: [{ title: 'In Person Session - Please Contact' }, { title: 'Discounted Further Sessions' }],
   },
   {
-    license: 'Single Session',
-    stripeName: 'Single Session',
-    price: '185',
-    commons: ['Book a Discovery Call First', '60 minute online - $185', '90 minute online - $245', 'Online Only - In Person can be arranged', 'Personalised session audio', 'Personalised session notes', 'Personalised Session tasking',],
-    options: [{ title: 'In Person Session - Please Contact' }, { title: 'Discounted Further Sessions' }],
-  },
-  {
     license: '3-Session Bundle',
     stripeName: '3-Session Bundle',
     price: '435',
     commons: ['Book a Discovery Call First', '60 minutes each', 'Only $145 per session - Save $120', 'Online Only - In Person can be arranged', '3x Personalised audios', 'Personalised session notes', 'Online or in person sessions', 'Personalised Session tasking'],
     options: [{ title: 'Single Sessions can be transferred' },],
   },
+  {
+    license: '90min Hypnotherapy',
+    stripeName: 'Single Session',
+    price: '245',
+    commons: ['Book a Discovery Call First', '90 minute online - $245', 'Online Only - In Person can be arranged', 'Personalised session audio', 'Personalised session notes', 'Personalised Session tasking',],
+    options: [{ title: 'In Person Session - Please Contact' }, { title: 'Discounted Further Sessions' }],
+  },
+
   // {
   //  license: 'TRANCEformd Program',
   //  stripeName: 'TRANCEformd Program',
