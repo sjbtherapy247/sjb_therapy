ebd13facc6d57cb99dc079c88cf41bbcc1a95535
sjbtherapy247 <sjbtherapy247@gmail.com>
2024-04-18 11:58:11 +1000

    sitemap...

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,49 +1,15 @@
-/** @type {import('next-sitemap').IConfig} */
-
-const config = {
+module.exports = {
   siteUrl: process.env.SITE_URL || 'https://sjbtherapy.com',
   generateRobotsTxt: false,
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 1000,
-  exclude: [],
-
-  // optional
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: []
-      },
-    ],
-    additionalSitemaps: [
-     
-    ],
-  },
-}
-
-export default config
-
-// next-sitemap.config.js
-
-// const { default: build } = require('next/dist/build');
-
-// ** @type {import('next-sitemap').IConfig} */
-
-// module.exports = {
-//  siteUrl: 'https://sjbtherapy.com',
-//  changefreq: 'daily',
-//  priority: 0.7,
-//  sitemapSize: 5000,
-//  generateRobotsTxt: true,
-//  exclude: ['/account','/auth'], // <= exclude here
-//  robotsTxtOptions: {
-//      userAgent: '*',
-//      allow: '/',
-  
-//  additionalSitemaps: [
-//      '/sitemap-0.xml', // <==== Add here
-//    ],
-//  },
-// }
+  sitemapSize: 5000,
+  outDir: './public',
+  // eslint-disable-next-line arrow-body-style
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString()
+    };
+  }
+};
