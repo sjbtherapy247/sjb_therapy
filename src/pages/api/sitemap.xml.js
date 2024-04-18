cb41bb38fe661c841fc5f3c9718a150a744d57f8
sjbtherapy247 <sjbtherapy247@gmail.com>
2024-04-18 15:25:32 +1000

    sitemap...

---
+++ src/pages/api/sitemap.xml.js
@@ -0,0 +1,23 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
+
+const sitemapHandler = async (req, res) => {
+  const links = [
+    { url: '/', changefreq: 'daily', priority: 0.7 },
+    // Add more URLs dynamically based on your application
+  ];
+
+  const sitemapStream = new SitemapStream({ hostname: 'https://example.com' });
+
+  res.writeHead(200, {
+    'Content-Type': 'application/xml',
+  });
+
+  const xmlString = await streamToPromise(
+    Readable.from(links).pipe(sitemapStream)
+  ).then((data) => data.toString());
+
+  res.end(xmlString);
+};
+
+export default sitemapHandler;
