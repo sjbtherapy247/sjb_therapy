54730f9a3c57121e6eee9af5f6529911460b4de8
sjbtherapy247 <sjbtherapy247@gmail.com>
2024-10-11 00:06:01 +1100

    gtm update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import createEmotionServer from '@emotion/server/create-instance';
 import createEmotionCache from 'src/utils/createEmotionCache';
 import palette from 'src/theme/palette';
@@ -16,12 +17,19 @@ const Favicon = () => (
 
 const Meta = () => (
   <>
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="initial-scale=1, width=device-width" />
     <meta name="format-detection" content="telephone=no, date=no, email=no, address=no" />
     <meta name="description" content="Hypnosis & Hypnotherapy in Sydney. Quit smoking or Vaping, Weight loss, Anxiety, Addiction, Phobia, Low Confidence, or More Performance. Book Online Today." />
     <meta name="keywords" content="Simon Baker The Hypnotherapist - Clinical Hypnotherapy Sydney" />
     <meta name="theme-color" content={palette('light').primary.dark} />
     <meta name="google-adsense-account" content="ca-pub-4250297759847171" />
+    {/* Open Graph meta tags for social media */}
+    <meta property="og:title" content="Simon Baker - Clinical Hypnotherapy in Sydney" />
+    <meta property="og:description" content="Quit smoking, lose weight, or improve your life with hypnotherapy. Book online with Simon Baker." />
+    <meta property="og:image" content="/assets/images/simon/simon-baker-main-sjbtherapy.webp" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.sjbtherapy.com" />
   </>
 );
 
@@ -31,11 +39,35 @@ export default function MyDocument({ emotionStyleTags }) {
       <Head>
         <Favicon />
         <Meta />
-        {/* Emotion */}
-        <meta name="emotion-insertion-point" content="" />
         {emotionStyleTags}
+
+        {/* Google Tag Manager - Head */}
+        <Script
+          id="gtm-script-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KWSDD4C');
+            `,
+          }}
+        />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) - Body */}
+        <noscript>
+          <iframe
+            title="Google Tag Manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KWSDD4C"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
