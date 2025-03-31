a7ce5e0c800be571588240d5a83437f0c5c174ae
sjbtherapy247 <sjbtherapy247@gmail.com>
2025-03-31 19:32:12 +1100

    Sitemaps

--- src/sections/_simo/next-sitemap.config.js
+++ src/sections/_simo/next-sitemap.config.js
@@ -2,10 +2,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://sjbtherapy.com', // Replace with your domain
     generateRobotsTxt: true, // (optional)
-    exclude: ['/server-sitemap.xml'], // <= exclude static pages
-    robotsTxtOptions: {
-      additionalSitemaps: [
-        `${process.env.SITE_URL}/server-sitemap.xml`, // <==== Add dynamic sitemaps
-      ],
-    },
+    
   };
